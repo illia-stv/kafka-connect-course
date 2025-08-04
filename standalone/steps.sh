65b5d57d414ba27a97f23992573f0a0fbd9e05dd
Illia Sheremetov <ilya@sheremetov.com>
2025-08-04 18:15:31 +0200

    Some minor changes.

--- standalone/steps.sh
+++ standalone/steps.sh
@@ -1,4 +1,10 @@
 # ----------------------------------------------------------------------------------------
+# ▶ Start Docker containers with rebuild
+# Builds images (if needed) and starts all services defined in the docker-compose.yml file.
+# The "-d" flag runs containers in detached mode.
+docker compose up --build -d
+
+# ----------------------------------------------------------------------------------------
 # ▶ Access Kafka container shell
 # Opens an interactive bash shell session inside the running Kafka container.
 docker exec -it kafka bash
@@ -9,4 +15,8 @@ docker exec -it kafka bash
 # The "--from-beginning" flag ensures it reads all existing messages from the beginning.
 kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic file-input --from-beginning
 
+# ----------------------------------------------------------------------------------------
+# ▶ Stop and remove Docker containers and volumes
+# Shuts down all running containers and removes their associated volumes.
+docker compose down -v
 # ----------------------------------------------------------------------------------------
